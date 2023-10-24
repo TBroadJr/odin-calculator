8cd6e204c334ca4f2e74e8a2de45d25bb48245f9
Tornelius Broadwater jr <tkbjr01@icloud.com>
2023-10-24 17:02:31 -0400

    Added calculator functionality

--- script.js
+++ script.js
@@ -1,43 +1,174 @@
-let firstOperator;
-let operation;
-let secondOperator;
+const displayItem = document.querySelector('.output');
+
+let firstOperator = ``;
+let operation = ``;
+let secondOperator = ``;
+
+let total;
+let isNegative = false
 
 
-function add(a, b) {
-  return a + b;
+function addButnAction() {
+  let butns = document.querySelectorAll('button');
+
+  for(let i = 0; i < butns.length; i++) {
+    let currentButn = butns[i];
+    let butnValue = currentButn.textContent;
+    currentButn.addEventListener('click', () => {
+      handleButnSelection(butnValue);
+    });
+  }
 }
 
-function subtrack(a, b) {
-  return a - b;
-}
+function handleButnSelection(val) {
+  switch (val) {
 
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-function operate(a, b, c) {
-
-  switch (b) {
-    case '+':
-      add(a, b);
+    case 'C':
+      clearSelected();
+      break;
+    
+    case '+/-':
+      toggleSign();
       break;
 
-    case '-':
-      subtrack(a, b);
-      break;
-
-    case '*':
-      multiply(a, b);
+    case '%':
+      operation = '%'
       break;
 
     case '/':
-      divide(a, b);
+      operation = '/'
+      break;
+
+    case 'X':
+      operation = '*'
+      break;
+    
+    case '-':
+      operation = '-'
+      break;
+
+    case '+':
+      operation = '+'
+      break;
+
+    case '=':
+      equalSelected();
+      break;
+
+    default:
+      numberSelected(val);
       break;
   }
 
 }
+
+function toggleSign() {
+  if (isNegative) {
+    isNegative = false;
+    if (operation === '') {
+      firstOperator = firstOperator.slice(1, firstOperator.length);
+      displayItem.textContent = firstOperator;
+    } else {
+      secondOperator = secondOperator.slice(1, secondOperator.length);
+      displayItem.textContent = secondOperator;
+    } 
+  } else {
+    isNegative = true;
+    if (operation === '') {
+      firstOperator = '-' + firstOperator;
+      displayItem.textContent = firstOperator;
+    } else {
+      secondOperator = '-' + secondOperator;
+      displayItem.textContent = secondOperator;
+    }
+  }
+}
+
+function clearSelected() {
+  displayItem.textContent = ``;
+  firstOperator = ``;
+  secondOperator = ``;
+  operation = ``;
+}
+
+
+function numberSelected(val) {
+  if (operation === ``) {
+    if (val === '.') {
+      if (!firstOperator.includes('.')) {
+        firstOperator += val;
+        displayItem.textContent = firstOperator;
+      }
+    } else {
+      firstOperator += val;
+      displayItem.textContent = firstOperator;
+    }
+  } else {
+    if (val === '.') {
+      if (!secondOperator.includes('.')) {
+        secondOperator += val;
+        displayItem.textContent = secondOperator;
+      }
+    } else {
+      secondOperator += val;
+      displayItem.textContent = secondOperator;
+    }
+  }
+}
+
+function equalSelected() {
+  switch (operation) {
+
+    case '+':
+      total = Number(firstOperator) + Number(secondOperator);
+      total = total.toFixed(5)
+      displayItem.textContent = `${total}`;
+      firstOperator = `${total}`;
+      secondOperator = '';
+      operation = '';
+      break;
+
+    case '-':
+      total = Number(firstOperator) - Number(secondOperator);
+      total = total.toFixed(5)
+      displayItem.textContent = `${total}`;
+      firstOperator = `${total}`;
+      secondOperator = ``;
+      operation = ``;
+      break;
+
+    case '/':
+      total = Number(firstOperator) / Number(secondOperator);
+      total = total.toFixed(5)
+      displayItem.textContent = `${total}`;
+      firstOperator = `${total}`;
+      secondOperator = ``;
+      operation = ``;
+      break;
+
+    case '*':
+      total = Number(firstOperator) * Number(secondOperator);
+      total = total.toFixed(5)
+      displayItem.textContent = `${total}`;
+      firstOperator = `${total}`;
+      secondOperator = ``;
+      operation = ``;
+      break;
+         
+    case '%':
+      total = Number(firstOperator) % Number(secondOperator);
+      total = total.toFixed(5)
+      displayItem.textContent = `${total}`;
+      firstOperator = `${total}`;
+      secondOperator = ``;
+      operation = ``;
+      break;
+  
+    default:
+      break;
+  }
+}
+
+addButnAction()
+
 
